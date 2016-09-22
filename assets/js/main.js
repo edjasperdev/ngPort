b3fa5380e8dbcb65931ef7e6d123966cebfc7c0f
Edward Jasper <edwardjasperjr@gmail.com>
2016-09-22 00:02:03 -0400

    remove angular

---
+++ assets/js/main.js
@@ -0,0 +1,99 @@
+var $ = jQuery.noConflict();
+
+(function($) {
+  'use strict';
+    
+    var $html = $('html');
+    var $body = $('body');
+    
+    
+/*=================================================
+Switch Animation
+=================================================*/
+
+function fn_switchAnimation() {
+
+  $('a[data-link-to]').on('click', function(e) {
+      alert('hey');
+    e.preventDefault();
+    var $this = $(this);
+    var $activeSection = $('section.active');
+    var $linkToSection = $($this.data('link-to'));
+    var id = $linkToSection.attr('id');
+    var $linkTomenuListItem = $('.mainNav').find('a[data-link-to="#' + id + '"]');
+    var $preloader = $('#preloader');
+    var $sectionWrap = $('.section-wrap');
+    var $menuListItem = $('#menu').find('li');
+    var $activeMenuListItem = $('#menu').find('.active');
+
+    if (!$linkToSection.hasClass('active')) {
+      $body.removeClass('menu-in');
+      $menuListItem.velocity('transition.slideUpOut', {stagger: 80});
+      $menuWrap.velocity('stop', true)
+      .velocity('fadeOut', {
+        duration: 500
+      });
+
+      $activeSection
+      .velocity('stop', true)
+      .velocity('transition.slideUpOut', {
+        duration: 1200,
+        complete: function() {
+          $preloader.velocity('fadeIn', {
+            duration: 800,
+            complete: function() {
+              $preloader.velocity('fadeOut', {
+                duration: 800,
+                delay: 100,
+                complete: function() {
+                  $linkToSection.velocity('transition.slideDownIn', {duration: 1000});
+                  $activeSection.add($activeMenuListItem).removeClass('active');
+                  $linkToSection.add($linkTomenuListItem).addClass('active');
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+
+/*=================================================
+preloader
+=================================================*/
+var _preloaderDuration = 800; // duration
+var _preloaderDelay = 500; // delay
+
+  function fn_preloader() {
+    var $preloader = $('#preloader');
+    $preloader.velocity('fadeOut', {
+      delay: _preloaderDelay,
+      duration: _preloaderDuration,
+      complete: function() {
+        $('section.active')
+        .velocity('stop', true)
+        .velocity('transition.slideDownIn', {duration: 2000});
+      }
+    });
+  }
+/*=================================================
+window on load
+=================================================*/
+  $(window).on('load', function() {
+
+    $('section').hide(); // hide all section
+        fn_preloader();
+
+  });
+
+/*=================================================
+document on ready
+=================================================*/
+  $(document).on('ready', function() {
+    fn_switchAnimation();
+  });
+    
+})(jQuery);
+
---
+++ assets/js/portfolio.js
@@ -0,0 +1,62 @@
+//var app = angular.module('ngApp');
+//
+//app.controller('PortCtrl', function ($scope) {
+//	var portProjects = [{
+//						project: "Barneys New York",
+//						description: "Created Web app to organize tagging documention, with password authentication and the other fun stuff like that",
+//						technology:"AngularJs, Bootstrap, Html ",
+//						link: "http://barneys.com",
+//						thumbnailLink:"images/port1.jpg"
+//					 },
+//					 {
+//						project: "Macros App",
+//						description: "Now we can all stay on track",
+//						technology:"ReactJS, Node, Html ",
+//						link: "http://nike.com",
+//						thumbnailLink:"images/port2.jpg"
+//
+//					 },
+//					 {
+//						project: "Fun Project",
+//						description: "are we having fun yet",
+//						technology:"AngularJs, Bootstrap, Html ",
+//						link: "http://barneyswarehouse.com",
+//						thumbnailLink:"images/port3.jpg"
+//					 },
+//					 {
+//						project: "Woohoo",
+//						description: "are we having fun yet",
+//						technology:"AngularJs, Bootstrap, Html ",
+//						link: "http://barneyswarehouse.com",
+//						thumbnailLink:"images/port3.jpg"
+//					 }
+//]	
+//
+//	
+//
+//
+//
+//var i = 1,
+//    l = portProjects.length;
+//    $scope.previousProject = portProjects[i-1];
+//	$scope.currentProject = portProjects[i]; 
+//	$scope.upcomingProject = portProjects[i+1];
+//	
+// 	$scope.nextProject = function(){
+//
+// 		//$('.prev').switchClass( "prev", "next", 1000, "easeInOutQuad" );
+// 		$('.current').switchClass( "current", "prev", 1000, "easeInOutQuad" );
+// 		$('.next').switchClass( "next", "current", 1000, "easeInOutQuad" );
+// 		
+// 	// 	i+=1;
+//  //   	$scope.previousProject = portProjects[(i+l-1)%l];
+//		// $scope.currentProject = portProjects[i>l-1?i=0:i];
+//		// $scope.upcomingProject = portProjects[(i+1)%l];
+//
+//
+// 		
+//
+//		
+// 	}
+//
+//});
